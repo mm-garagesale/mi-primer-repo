ad2e8d8bc6033356d2d5506c0ce38179cb3bd238
Martin <116912240+mm-garagesale@users.noreply.github.com>
2023-09-01 22:04:28 -0300

    Agregué una linea de debug con un mensaje en ScriptLocoDeUnDev.cs

--- Assets/Scripts/ScriptLocoDeUnDev.cs
+++ Assets/Scripts/ScriptLocoDeUnDev.cs
@@ -7,7 +7,7 @@ public class ScriptLocoDeUnDev : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
